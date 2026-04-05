ede1174c6e4a18fb16db4647b0fcd196e539bdba
Dhruthishreenarayan <dhruthishreenarayan28@gmail.com>
2026-04-05 20:53:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (80.13%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+       if not matrix or not matrix[0]:
+         return False
+       m=len(matrix)
+       n=len(matrix[0])
+       sr=0
+       er=n*m-1
+       while sr <=  er:
+         mid=(sr+er)/2
+         r=mid//n
+         c=mid%n
+         if matrix[r][c]==target:
+            return True
+         elif matrix[r][c] < target :
+            sr=mid+1
+         else:
+            er=mid-1
+       return False
+        
