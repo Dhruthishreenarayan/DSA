246cddd8ba2d7bd6aaab8b20c244db3f0010f789
Dhruthishreenarayan <dhruthishreenarayan28@gmail.com>
2026-04-08 19:18:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (49.09%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseWords(self, s):
+       return " ".join(s.split()[::-1])     
+                    
+
+            
+                
+
+            
+            
+        
