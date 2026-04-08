c102876405a2205f85ee1a46841cca35bfec806f
Dhruthishreenarayan <dhruthishreenarayan28@gmail.com>
2026-04-08 19:38:16 +0530

    Time: 21 ms (33.13%) | Memory: 12.6 MB (25.77%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeOccurrences(self, s, part):
+       stack=[]
+       k=len(part)
+       for ch in s:
+        stack.append(ch)
+        if len(stack)>=k and "".join(stack[-k:])==part:
+            for i in range(k):
+                stack.pop()
+       return "".join(stack)
+        
