bccb1f0c27bd81474d1fd3ec2d1a9487e4919930
Dhruthishreenarayan <dhruthishreenarayan28@gmail.com>
2026-05-19 11:54:36 +0530

    Time: 3 ms (63.12%) | Memory: 12.5 MB (21.51%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def myAtoi(self, s):
+        s=s.lstrip()  #removes extra whitespaces
+        if not s:
+            return 0
+        sign=1
+        i=0
+        if s[i]=='-':
+            sign=-1
+            i+=1
+        elif s[i]=='+':
+            i+=1
+        res=0
+        while i<len(s) and s[i].isdigit():
+            digit=int(s[i])
+            if res>(2**31 - 1 - digit)//10:
+                if sign==1:
+                    return 2**31-1
+                else:
+                    return -2**31
+            res = res * 10 + digit
+            i+=1
+        return sign*res
+        
